50f669cc52c0e70de410066e50eff6dc0ddfac88
Ricardo.lagos <ricardolagoshn>
2022-03-15 20:05:54 -0600

    Add project files.

---
+++ PM02MVVM12/Models/Ordenes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM02MVVM12.Models
+{
+    public class Ordenes
+    { 
+        public int id { set; get; }
+
+        public string NombreCliente { set; get; }
+
+        public double MontoTotal { set; get; }
+
+        public DateTime fecha { set; get; }
+
+        public string status { set; get; }
+    }
+}
---
+++ PM02MVVM12/ViewModels/OrdenesViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace PM02MVVM12.ViewModels
+{
+    public class OrdenesViewModel : BaseViewModels
+    {
+        private string _nombre;
+        private double _monto;
+        private DateTime _fecha;
+        private string _status;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; OnPropertyChanged(); }
+        }
+
+        public double Monto
+        {
+            get { return _monto; }
+            set { _monto = value; OnPropertyChanged(); }
+        }
+
+        public DateTime Fecha
+        {
+            get { return _fecha; }
+            set { _fecha = value; OnPropertyChanged(); }
+        }
+
+        public string Status 
+        {
+            get { return _status; }
+            set { _status = value; OnPropertyChanged(); }
+        }
+
+        /*  Comandos a ser utlizados desde el View  */
+
+        public ICommand CleanCommand { private get; set; }
+        public ICommand CreateCommand { private get; set; }
+        public ICommand  GetCommand { private get; set; }
+        public ICommand PutCommand { private get; set; }
+        public ICommand DelCommand { private get; set; }
+
+
+    }
+}
